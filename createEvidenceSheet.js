2266155c2cd4e795b2c02cc2309e04e45edea3ae
abi-shimoji <kaito.shimoji@abi-inc.co.jp>
2025-01-15 09:59:51 +0900

    refact: 結合セルの値取得処理を修正

--- createEvidenceSheet.js
+++ createEvidenceSheet.js
@@ -6,6 +6,15 @@ function createEvidenceSheet() {
 
   const latestRowNo = sheet.getLastRow();
   const testFinalNo = latestRowNo - TEST_NO_SHEET_ROW_GAP;
+  // 結合セルから値を取得する関数
+  const getCellValue = (targetCellColumn, targetCellRowNum) => {
+    let result = "";
+    for (let i = targetCellRowNum; TEST_NO_SHEET_ROW_GAP !== i + TEST_NO_SHEET_ROW_GAP; i--) {
+      result = sheet.getRange(targetCellColumn + (i + TEST_NO_SHEET_ROW_GAP)).getValue();
+      if (result) break;
+    }
+    return result;
+  };
 
   for (let i = 1; i <= testFinalNo; i++) {
     const newSheetName = `エビデンス${i}`;
@@ -15,53 +24,16 @@ function createEvidenceSheet() {
     }
 
     // テスト項目詳細取得
-    let cat1 = sheet.getRange(`C${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 分類1
-    let cat2 = sheet.getRange(`D${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 分類2
-    let cat3 = sheet.getRange(`E${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 分類3
-    let prerequisites = sheet.getRange(`F${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 前提手順
-    let operatigIns = sheet.getRange(`G${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 操作手順
-    let expectedResults = sheet.getRange(`H${i + TEST_NO_SHEET_ROW_GAP}`).getValue(); // 期待結果
-
-    if (!cat1) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        cat1 = sheet.getRange(`C${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (cat1) break;
-      }
-    }
-    if (!cat2) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        cat2 = sheet.getRange(`D${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (cat2) break;
-      }
-    }
-    if (!cat3) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        cat3 = sheet.getRange(`E${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (cat3) break;
-      }
-    }
-    if (!prerequisites) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        prerequisites = sheet.getRange(`F${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (prerequisites) break;
-      }
-    }
-    if (!operatigIns) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        operatigIns = sheet.getRange(`G${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (operatigIns) break;
-      }
-    }
-    if (!expectedResults) {
-      for (let j = i - 1; TEST_NO_SHEET_ROW_GAP !== j + TEST_NO_SHEET_ROW_GAP; j--) {
-        expectedResults = sheet.getRange(`H${j + TEST_NO_SHEET_ROW_GAP}`).getValue();
-        if (expectedResults) break;
-      }
-    }
+    let cat1 = getCellValue('C', i); // 分類1
+    let cat2 = getCellValue('D', i); // 分類2
+    let cat3 = getCellValue('E', i); // 分類3
+    let prerequisites = getCellValue(`F`, i); // 前提手順
+    let operatigIns = getCellValue(`G`, i); // 操作手順
+    let expectedResults = getCellValue(`H`, i); // 期待結果
 
     // シート追加
     const newSheet = spreadSheet.insertSheet(newSheetName);
-    
+
     // 追加したシートを最後尾に移動
     const sheets = spreadSheet.getSheets();
     spreadSheet.setActiveSheet(newSheet);
@@ -83,7 +55,7 @@ function createEvidenceSheet() {
 
     // 書式設定
     const testItemTitleRange = newSheet.getRange("B2:B7");
-    testItemTitleRange.setBackground('#00af98');
+    testItemTitleRange.setBackground("#00af98");
     testItemTitleRange.setBorder(true, true, true, true, true, true);
     const testItemValueRange = newSheet.getRange("C2:C7");
     testItemValueRange.setBorder(true, true, true, true, true, true);
